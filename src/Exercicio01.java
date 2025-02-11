4dce087a3767e96f4de66aa317982551bc8851c3
Nicolas Ruan <105995275+NicoolasRuan@users.noreply.github.com>
2025-02-10 21:03:15 -0300

    exercicio_01

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
-        System.out.println("Ollaa");
+        // 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B e mostre se a soma é menor que C
 
-        System.out.println("testando");
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, soma;
+
+        System.out.print("valor A: ");
+        A = sc.nextInt();
+
+        System.out.print("valor B: ");
+        B = sc.nextInt();
+
+        System.out.print("valor C: ");
+        C = sc.nextInt();
+        soma = A + B;
+
+        System.out.println("soma A e B = " + soma);
+        System.out.println("soma menor que C? = " + (soma < C ? "SIM" : "NÃO"));
+
+        sc.close();
+
     }
 
 }
